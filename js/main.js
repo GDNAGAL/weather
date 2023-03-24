2e0018475ee5c6a1910491b18cc43bea4cfb31b1
GDNAGAL <gdnagal1536@gmail.com>
2023-03-24 12:48:46 +0530

    updated

--- js/main.js
+++ js/main.js
@@ -1,14 +1,40 @@
 const findWether = document.querySelector("#findWether");
 const forecast_container = document.querySelector(".forecast-container");
 
+// factch data from api
+const getdata = async function () {
+  try {
+    const factchapi = await fetch(
+      `http://api.weatherapi.com/v1/forecast.json?key=776313bc8d2245078a660229232403&q=Bikaner&days=7&aqi=yes&alerts=yes`
+    );
+    const data = await factchapi.json();
+    // console.log(data);
+    return data;
+  } catch (err) {
+    // console.log(err);
+  }
+};
+async function temp1() {
+  const getdata2 = await getdata();
+  const temp2 = getdata2.forecast.forecastday;
+  // console.log(temp2);)
+  return temp2;
+}
+const temp4 = Promise.resolve(temp1()).then((value) => {
+  for (d = 0; d < 7; d++) {
+    forecast_container.insertAdjacentHTML("beforeend", forecastPrint(value[d]));
+  }
+});
+
 function forecastPrint(date) {
-  return `<div class="forecast ${date == day ? "today" : ""}">
+  console.log(date);
+  return `<div class="forecast ${date == 0 ? "today" : ""}">
   <div class="forecast-header">
     <div class="day">Monday</div>
-    <div class="date">${date}</div>
+    <div class="date">${date.date}</div>
   </div>
   <div class="forecast-content">
-    <div class="location">New York</div>
+    <div class="location">Bikaner</div>
     <div class="degree">
       <div class="num">23<sup>o</sup>C</div>
       <div class="forecast-icon">
@@ -28,20 +54,19 @@ function forecastPrint(date) {
 </div>`;
 }
 
-const date = new Date();
-let day = date.getDate();
-for (d = 0; d < 7; d++) {
-  forecast_container.insertAdjacentHTML("beforeend", forecastPrint(day + d));
-}
+// for (d = 0; d < 7; d++) {
+//   forecast_container.insertAdjacentHTML("beforeend", forecastPrint(temp4[d]));
+// }
 
-console.log(forecastPrint);
-const forecast = document.querySelectorAll(".forecast");
+// console.log(forecastPrint);
 
-forecast.forEach((item, index) => {
+const foreCast = document.querySelectorAll(".forecast");
+
+foreCast.forEach((item, index) => {
   item.addEventListener("click", function () {
-    for (i = 0; i < forecast.length; i++) {
-      forecast[i].classList.remove("today");
+    for (i = 0; i < foreCast.length; i++) {
+      foreCast[i].classList.remove("today");
     }
-    forecast[index].classList.toggle("today");
+    foreCast[index].classList.toggle("today");
   });
 });

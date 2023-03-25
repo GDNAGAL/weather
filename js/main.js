009d972cc287221fc0c1027a8561830ca0d47e4a
GDNAGAL <gdnagal1536@gmail.com>
2023-03-25 11:02:12 +0530

    updated

--- js/main.js
+++ js/main.js
@@ -1,72 +1,74 @@
-const findWether = document.querySelector("#findWether");
 const forecast_container = document.querySelector(".forecast-container");
+const dat = new Date();
+const todaydate = dat.toISOString().slice(0, 10);
+const formEl = document.querySelector("#find-location");
 
 // factch data from api
-const getdata = async function () {
+const getdata = async function (cityLocation = "Bikaner") {
   try {
     const factchapi = await fetch(
-      `http://api.weatherapi.com/v1/forecast.json?key=776313bc8d2245078a660229232403&q=Bikaner&days=7&aqi=yes&alerts=yes`
+      `http://api.weatherapi.com/v1/forecast.json?key=776313bc8d2245078a660229232403&q=${cityLocation}&days=7&aqi=yes&alerts=yes`
     );
     const data = await factchapi.json();
-    // console.log(data);
-    return data;
+    const response_forecastday = data.forecast.forecastday;
+    console.log(response_forecastday);
+    for (d = 0; d < 7; d++) {
+      forecast_container.insertAdjacentHTML(
+        "beforeend",
+        forecastPrint(response_forecastday[d], cityLocation)
+      );
+    }
+    const foreCast = document.querySelectorAll(".forecast");
+    foreCast.forEach((item, index) => {
+      item.addEventListener("click", function () {
+        for (i = 0; i < foreCast.length; i++) {
+          foreCast[i].classList.remove("today");
+        }
+        foreCast[index].classList.toggle("today");
+      });
+    });
   } catch (err) {
-    // console.log(err);
+    alert("Please Check City Name ! " + err);
   }
 };
-async function temp1() {
-  const getdata2 = await getdata();
-  const temp2 = getdata2.forecast.forecastday;
-  // console.log(temp2);)
-  return temp2;
-}
-const temp4 = Promise.resolve(temp1()).then((value) => {
-  for (d = 0; d < 7; d++) {
-    forecast_container.insertAdjacentHTML("beforeend", forecastPrint(value[d]));
-  }
-});
 
-function forecastPrint(date) {
-  console.log(date);
-  return `<div class="forecast ${date == 0 ? "today" : ""}">
+// print forecast
+function forecastPrint(date, cityLocation) {
+  // console.log(date);
+  return `<div class="forecast ${date.date == todaydate ? "today" : ""}">
   <div class="forecast-header">
     <div class="day">Monday</div>
     <div class="date">${date.date}</div>
   </div>
   <div class="forecast-content">
-    <div class="location">Bikaner</div>
+    <div class="location">${cityLocation}</div>
     <div class="degree">
-      <div class="num">23<sup>o</sup>C</div>
+      <div class="num">${date.day.avgtemp_c}<sup>o</sup>C</div>
       <div class="forecast-icon">
-        <img src="images/icons/icon-1.svg" alt="" class="icon-f" />
+        <img src="${date.day.condition.icon}" alt="${
+    date.day.condition.text
+  }" class="icon-f" />
       </div>
     </div>
     <span class="moredetail">
-      <img src="images/icon-umberella.png" alt="" />20%
+      <img src="images/icon-wind.png" alt="" />${date.day.maxwind_kph} km/h
     </span>
     <span class="moredetail">
-      <img src="images/icon-wind.png" alt="" />18km/h
+      S-rise : ${date.astro.sunrise}
     </span>
     <span class="moredetail">
-      <img src="images/icon-compass.png" alt="" />East
+      S-set : ${date.astro.sunset}
     </span>
   </div>
 </div>`;
 }
 
-// for (d = 0; d < 7; d++) {
-//   forecast_container.insertAdjacentHTML("beforeend", forecastPrint(temp4[d]));
-// }
-
-// console.log(forecastPrint);
-
-const foreCast = document.querySelectorAll(".forecast");
-
-foreCast.forEach((item, index) => {
-  item.addEventListener("click", function () {
-    for (i = 0; i < foreCast.length; i++) {
-      foreCast[i].classList.remove("today");
-    }
-    foreCast[index].classList.toggle("today");
-  });
+//submit form
+formEl.addEventListener("submit", function () {
+  const cityval = document.querySelector("#cityval").value;
+  if (!cityval) return;
+  forecast_container.innerHTML = "";
+  document.querySelector("#cityval").value = "";
+  // return inputval;
+  getdata(cityval);
 });
